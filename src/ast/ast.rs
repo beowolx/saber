dcf36cb39bd409fdb7862fd71fad0fb87fd8241b
beowulf <61982523+beowolx@users.noreply.github.com>
2023-05-30 08:46:45 +0200

    feat(parser): created basic AST representation

---
+++ src/ast/ast.rs
@@ -0,0 +1,58 @@
+use crate::token::Token;
+
+pub trait Node {
+  fn token_literal(&self) -> String;
+}
+
+pub trait Statement: Node {
+  fn statement_node(&self);
+}
+
+pub trait Expression: Node {
+  fn expression_node(&self);
+}
+
+pub struct Program {
+  pub statements: Vec<Box<dyn Statement>>,
+}
+
+impl Node for Program {
+  fn token_literal(&self) -> String {
+    if self.statements.len() > 0 {
+      self.statements[0].token_literal()
+    } else {
+      String::from("")
+    }
+  }
+}
+
+pub struct VarStatement {
+  pub token: Token,
+  pub name: Identifier,
+  pub value: Box<dyn Expression>,
+}
+
+impl Statement for VarStatement {
+  fn statement_node(&self) {}
+}
+
+impl Node for VarStatement {
+  fn token_literal(&self) -> String {
+    self.token.literal.clone()
+  }
+}
+
+pub struct Identifier {
+  pub token: Token,
+  pub value: String,
+}
+
+impl Expression for Identifier {
+  fn expression_node(&self) {}
+}
+
+impl Node for Identifier {
+  fn token_literal(&self) -> String {
+    self.token.literal.clone()
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+mod ast;
 mod lexer;
 mod repl;
 mod token;
