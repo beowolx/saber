577e5be65a7f2c2dae3f65a25061a3e933f8108b
beowulf <61982523+beowolx@users.noreply.github.com>
2023-07-11 08:55:34 +0200

    feat(parser): implement basic ast for statement expressions

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -2,6 +2,7 @@ use crate::token::Token;
 
 pub trait Node {
   fn token_literal(&self) -> String;
+  fn string(&self) -> String;
 }
 
 pub trait Statement: Node {
@@ -32,6 +33,16 @@ impl Node for Program {
       String::from("")
     }
   }
+
+  fn string(&self) -> String {
+    let mut out = String::new();
+
+    for s in &self.statements {
+      out.push_str(&s.string());
+    }
+
+    out
+  }
 }
 
 pub struct VarStatement {
@@ -48,6 +59,22 @@ impl Node for VarStatement {
   fn token_literal(&self) -> String {
     self.token.literal.clone()
   }
+  fn string(&self) -> String {
+    let mut out = String::new();
+
+    out.push_str(&self.token_literal());
+    out.push_str(" ");
+    out.push_str(&self.name.value);
+    out.push_str(" = ");
+
+    if let Some(value) = &self.value {
+      out.push_str(&value.string());
+    }
+
+    out.push_str(";");
+
+    out
+  }
 }
 
 pub struct Identifier {
@@ -63,19 +90,93 @@ impl Node for Identifier {
   fn token_literal(&self) -> String {
     self.token.literal.clone()
   }
+  fn string(&self) -> String {
+    self.value.clone()
+  }
 }
 
 pub struct RetStatement {
-    pub token: Token,
-    pub return_value: Option<Box<dyn Expression>>,
+  pub token: Token,
+  pub return_value: Option<Box<dyn Expression>>,
 }
 
 impl Statement for RetStatement {
-    fn statement_node(&self) {}
+  fn statement_node(&self) {}
 }
 
 impl Node for RetStatement {
-    fn token_literal(&self) -> String {
-        self.token.literal.clone()
+  fn token_literal(&self) -> String {
+    self.token.literal.clone()
+  }
+
+  fn string(&self) -> String {
+    let mut out = String::new();
+
+    out.push_str(&self.token_literal());
+    out.push_str(" ");
+
+    if let Some(value) = &self.return_value {
+      out.push_str(&value.string());
     }
+
+    out.push_str(";");
+
+    out
+  }
+}
+
+pub struct ExpressionStatement {
+  pub token: Token,
+  pub expression: Option<Box<dyn Expression>>,
+}
+
+impl Statement for ExpressionStatement {
+  fn statement_node(&self) {}
+}
+
+impl Node for ExpressionStatement {
+  fn token_literal(&self) -> String {
+    self.token.literal.clone()
+  }
+  fn string(&self) -> String {
+    if let Some(expr) = &self.expression {
+      expr.string()
+    } else {
+      String::new()
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::token::TokenType;
+
+  #[test]
+  fn test_string_value() {
+    let program = Program {
+      statements: vec![Box::new(VarStatement {
+        token: Token {
+          token_type: TokenType::Var,
+          literal: String::from("var"),
+        },
+        name: Identifier {
+          token: Token {
+            token_type: TokenType::Ident,
+            literal: String::from("myVar"),
+          },
+          value: String::from("myVar"),
+        },
+        value: Some(Box::new(Identifier {
+          token: Token {
+            token_type: TokenType::Ident,
+            literal: String::from("anotherVar"),
+          },
+          value: String::from("anotherVar"),
+        })),
+      })],
+    };
+
+    assert_eq!(program.string(), "var myVar = anotherVar;");
+  }
 }
