ed915f636e453bcc8d034a88db25275548de8d01
beowulf <61982523+beowolx@users.noreply.github.com>
2023-10-04 09:04:48 +0200

    refactor(tokens): refactor tokens

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -1,86 +1,46 @@
-pub const WEAVE: &str = "weave";
-pub const FORGE: &str = "forge";
-pub const TRUE: &str = "true";
-pub const FALSE: &str = "false";
-pub const IF: &str = "if";
-pub const ELSE: &str = "else";
-pub const IGNITE: &str = "ignite";
-
-pub const EQ: &str = "==";
-pub const NOT_EQ: &str = "!=";
-pub const ASSIGN: char = '=';
-pub const SEMICOLON: char = ';';
-pub const LPAREN: char = '(';
-pub const RPAREN: char = ')';
-pub const LBRACE: char = '{';
-pub const RBRACE: char = '}';
-pub const COMMA: char = ',';
-pub const PLUS: char = '+';
-pub const MINUS: char = '-';
-pub const BANG: char = '!';
-pub const ASTERISK: char = '*';
-pub const SLASH: char = '/';
-pub const LT: char = '<';
-pub const GT: char = '>';
-pub const EOF: char = '\0';
-
-#[derive(Debug, PartialEq, Clone, Eq, Hash)]
-pub enum TokenType {
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
   Illegal,
   Eof,
-  Ident,
-  Int,
+
+  // Identifiers + literals
+  Ident(String),
+  Int(i64),
+  String(String),
+  Boolean(bool),
+
+  // Statements
   Assign,
+  If,
+  Else,
+
+  // Operators
   Plus,
   Minus,
   Bang,
   Asterisk,
   Slash,
+
+  Equal,
+  NotEqual,
   Lt,
+  LessThanEqual,
   Gt,
+  GreaterThanEqual,
+
+  // Delimiters
   Comma,
+  Colon,
   Semicolon,
   Lparen,
   Rparen,
   Lbrace,
   Rbrace,
-  Function,
+  Lbracket,
+  Rbracket,
+
+  // Reseved keywords
+  Weave,
   Forge,
-  True,
-  False,
-  If,
-  Else,
   Ignite,
-  Eq,
-  NotEq,
-}
-
-impl TokenType {
-  pub fn lookup_ident(ident: &str) -> Self {
-    match ident {
-      WEAVE => Self::Function,
-      FORGE => Self::Forge,
-      TRUE => Self::True,
-      FALSE => Self::False,
-      IF => Self::If,
-      ELSE => Self::Else,
-      IGNITE => Self::Ignite,
-      _ => Self::Ident,
-    }
-  }
-}
-
-#[derive(PartialEq, Debug, Clone)]
-pub struct Token {
-  pub token_type: TokenType,
-  pub literal: String,
-}
-
-impl Token {
-  pub fn new(token_type: TokenType, literal: String) -> Self {
-    Self {
-      token_type,
-      literal,
-    }
-  }
 }
